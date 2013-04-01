083630d7048d30324c0f538c13913889f44277ce
Sam Phippen <samphippen@googlemail.com>
2013-04-01 19:53:28 +0100

    Do it
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

---
+++ hash.py
@@ -0,0 +1,36 @@
+import skein
+from gmpy import hamdist as hamming
+import binascii
+import http.client as httplib
+import urllib.parse as urllib
+import os
+
+urandom = open("/dev/urandom", "rb")
+def somebits():
+    return binascii.hexlify(urandom.read(100))
+
+if __name__ == "__main__":
+    original = int("5b4da95f5fa08280fc9879df44f418c8f9f12ba424b7757de02bbdfbae0d4c4fdf9317c80cc5fe04c6429073466cf29706b8c25999ddd2f6540d4475cc977b87f4757be023f19b8f4035d7722886b78869826de916a79cf9c94cc79cd4347d24b567aa3e2390a573a373a48a5e676640c79cc70197e1c5e7f902fb53ca1858b6", 16)
+    domain = "bristol.ac.uk"
+    best = 10000
+    i = 0
+    while True:
+        i += 1
+        if i % (1 << 16) == 0:
+            print(best)
+        bits = somebits()
+        sbits = "".join([chr(byte) for byte in bits])
+        test = skein.skein1024(bits).hexdigest()
+        as_bits = int(test, 16)
+        distance = hamming(as_bits, original)
+        if distance < best:
+            print( "-------------")
+            best = distance
+            print(best)
+            os.system('curl -s "http://almamater.xkcd.com/?edu=bristol.ac.uk" --data "hashable=' + sbits + '"')
+            print("")
+            print("")
+            print("")
+            print("")
+            print(test)
+            print("-------------")
---
+++ readme.md
@@ -0,0 +1,1 @@
+Just run hash.py with python3. You'll need the pyskein library and numpy and scipy.
